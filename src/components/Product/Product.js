3cd43b6c10396591ce8c6e0a18e69b0413cab50a
behrooz <behrooz.zendedel1992@gmail.com>
2022-02-18 20:36:23 +0330

    update product & productList in state =>handler

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,12 @@
 import styles from './Product.module.css'
 
 const Product = (props) => {
+ 
     return (
         <div className={styles.product} onClick={props.click}>
             <p>product name : {props.name} course</p>
             <p>product price : {props.price}</p>
-
+            <button onClick={props.onDelete} >delete</button>
         </div>
 
     );
--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -4,18 +4,31 @@ import Product from '../Product/Product';
 
 class ProductList extends Component {
     state = {
-        products:[
-            { title: "react.js", price: "99$",id:1 },
-            { title: "node.js", price: "89$",id:2 },
-            { title: "javascript", price: "79$" ,id:3},
-            ],
+        products: [
+            { title: "react.js", price: "99$", id: 1 },
+            { title: "node.js", price: "89$", id: 2 },
+            { title: "javascript", price: "79$", id: 3 },
+        ],
     }
 
+    // state => handler ! ghanone 3 angoshti ta betonim b state access dashte bashim
+
+   removeHandler = ()=>{
+        console.log('clicked') 
+
+
+     }
     render() {
         return (
             <div>
                 {this.state.products.map((product, index) => {
-                    return <Product name={product.title} price={product.price} key={index} click={this.clickHandler} />
+                    return <Product
+                        name={product.title}
+                        price={product.price}
+                        key={index}
+                        click={this.clickHandler} 
+                        onDelete={this.removeHandler}
+                        />
                 })}
             </div>
         );
