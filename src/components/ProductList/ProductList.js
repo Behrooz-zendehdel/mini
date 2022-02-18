4c3fee791ebba526690d3519ce123a678b108c61
behrooz <behrooz.zendedel1992@gmail.com>
2022-02-18 20:42:57 +0330

    update removeHandler in filter metod

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -13,11 +13,11 @@ class ProductList extends Component {
 
     // state => handler ! ghanone 3 angoshti ta betonim b state access dashte bashim
 
-   removeHandler = ()=>{
-        console.log('clicked') 
-
-
-     }
+    removeHandler = (id) => {
+        console.log('clicked', id)
+        const filterdProduct = this.state.products.filter((p) => p.id !== id);
+        this.setState({ products: filterdProduct })
+    }
     render() {
         return (
             <div>
@@ -26,9 +26,9 @@ class ProductList extends Component {
                         name={product.title}
                         price={product.price}
                         key={index}
-                        click={this.clickHandler} 
-                        onDelete={this.removeHandler}
-                        />
+                        click={this.clickHandler}
+                        onDelete={() => this.removeHandler(product.id)}
+                    />
                 })}
             </div>
         );
