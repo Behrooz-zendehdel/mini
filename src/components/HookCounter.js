893e2b75df0ee4a57dd4ff0f7685c94429b203cb
behrooz <behrooz.zendedel1992@gmail.com>
2022-02-18 16:58:05 +0330

    update state used callback function

--- src/components/HookCounter.js
+++ src/components/HookCounter.js
@@ -1,25 +1,29 @@
 import { useState } from "react";
 
 const HookCounter = () => {
-    const [count,setCount]=useState(0);
+    const [count, setCount] = useState(0);
 
-    const addOneHandler =()=>{
-        setCount(count+1);
+    const addOneHandler = () => {
+        setCount(count + 1);
     }
-    const addTowHandler =()=>{
-        setCount(count+2);
+    const addTowHandler = () => {
+        setCount(count + 2);
     }
-    const addFiveHandler =()=>{
-        setCount(count+5);
+    const addFiveHandler = () => {
+        // setCount(count+5);
+        for (let i = 0; i < 5; i++) {
+            setCount(prev => prev + 1)
+            // update state based on prev state => callback function!!!
+        }
     }
-    return ( 
+    return (
         <div>
             <h2>count - {count}</h2>
             <button onClick={addOneHandler}>add one</button>
             <button onClick={addTowHandler}>add Tow</button>
             <button onClick={addFiveHandler}>add Five</button>
         </div>
-     );
+    );
 }
- 
+
 export default HookCounter;
