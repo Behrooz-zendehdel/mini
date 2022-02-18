90d812fc21bd7b3229937c3ae0854a9f5b2a8f0f
behrooz <behrooz.zendedel1992@gmail.com>
2022-02-18 16:51:59 +0330

    add button to hookcounter

--- src/components/HookCounter.js
+++ src/components/HookCounter.js
@@ -6,10 +6,18 @@ const HookCounter = () => {
     const addOneHandler =()=>{
         setCount(count+1);
     }
+    const addTowHandler =()=>{
+        setCount(count+2);
+    }
+    const addFiveHandler =()=>{
+        setCount(count+5);
+    }
     return ( 
         <div>
             <h2>count - {count}</h2>
             <button onClick={addOneHandler}>add one</button>
+            <button onClick={addTowHandler}>add Tow</button>
+            <button onClick={addFiveHandler}>add Five</button>
         </div>
      );
 }
