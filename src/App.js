3766e8f1e1151ed5e0c162fde3f60add3d7eb3a6
behrooz <behrooz.zendedel1992@gmail.com>
2022-02-18 14:56:15 +0330

    add jsx cm app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//component:
+const App = () => {
+    return (
+        //jsx
+        <div className='container' id='title'>
+            <h1>shopping</h1>
+            <p>product #1 book :1</p>
+
+        </div>
+    )
+};
+
+export default App;
+// modular programing
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-const element = React.createElement('div', { id: 'title', className: 'app-title' }, "this is first app");
+import App from "./App";
+ React.createElement('div', { id: 'title', className: 'app-title' }, "this is first app");
 
-ReactDOM.render(element, document.getElementById("root"));
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
