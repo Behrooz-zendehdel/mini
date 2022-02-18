226d17e8b0472306ec4bc381774d0c8a2b50fadd
behrooz <behrooz.zendedel1992@gmail.com>
2022-02-18 15:26:42 +0330

    class cm App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import Product from './component/Product/Product';
 //component:
-const App = () => {
-    return (
-        //jsx
-        <div className='container' id='title'>
-            <h1>shopping</h1>
-            <Product name='react.js' price='99$' />
-            <Product name='node.js' price='89$' />
-            <Product name='javascript' price='79$'>
-               <p>discount 15%</p>
-            </Product>
-        </div>
-    )
-};
+// const App = () => {
+//     return (
+//         //jsx
+//         <div className='container' id='title'>
+//             <h1>shopping</h1>
+//             <Product name='react.js' price='99$' />
+//             <Product name='node.js' price='89$' />
+//             <Product name='javascript' price='79$'>
+//                <p>discount 15%</p>
+//             </Product>
+//         </div>
+//     )
+// };
 
-export default App;
-// modular programing
+// export default App;
+// // modular programing
+
+
+// class component App 
+class App extends React.Component {
+    render() {
+        return (
+            <div className='container' id='title'>
+                <h1>shopping</h1>
+                <Product name='react.js' price='99$' />
+                <Product name='node.js' price='89$' />
+                <Product name='javascript' price='79$'>
+                    <p>discount 15%</p>
+                </Product>
+            </div>
+        )
+    }
+}
+export default App
