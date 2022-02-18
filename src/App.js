c7f25a22e720e290e71eab728969337bae26011b
behrooz <behrooz.zendedel1992@gmail.com>
2022-02-18 16:35:38 +0330

    switch to functional component app

--- src/App.js
+++ src/App.js
@@ -1,58 +1,86 @@
-import React, { Component } from 'react';
+import React, { Component,useState } from 'react';
 import Product from './components/Product/Product';
 
 
 
-class App extends Component {
+const App = () => {
+  const [products,setProducts]=  useState([
+                    { title: "react.js", price: "99$" },
+                    { title: "node.js", price: "89$" },
+                    { title: "javascript", price: "79$" }
+                ]);
 
-    state = {
-        products: [
-            { title: "react.js", price: "99$" },
-            { title: "node.js", price: "89$" },
-            { title: "javascript", price: "79$" }
-        ],
-    };
+                const clickHandler =()=>{
 
-    clickHandler =()=> {
-        this.setState({
-                products:[
-                    { title: "react.js", price: "79$" },
-                    { title: "node.js", price: "69$" },
-                    { title: "javascript", price: "59$" },
-                ],
-        })
-    }
+                }
 
-    render() {
-        return (
-            <div className="container" id="title">
-                <h1>shopping</h1>
-                {this.state.products.map((product) => {
-                    return <Product name={product.title} price={product.price} />
-                })}
-                <button onClick={this.clickHandler}>change price</button>
-            </div >
-        );
-    }
-}
-export default App
+    return (
+        <div className="container" id="title">
+            <h1>shopping</h1>
+            {products.map((product) => {
+                return <Product name={product.title} price={product.price} />
+            })}
+            <button onClick={clickHandler}>change price</button>
+        </div >
+    );
+};
+
+export default App;
 
 
 
 
-// const App = () => {
-//     return (
-//         //jsx
-//         <div className='container' id='title'>
-//             <h1>shopping</h1>
-//             <Product name='react.js' price='99$' />
-//             <Product name='node.js' price='89$' />
-//             <Product name='javascript' price='79$'>
-//                <p>discount 15%</p>
-//             </Product>
-//         </div>
-//     )
-// };
 
-// export default App;
-// // modular programing
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class App extends Component {
+
+//     state = {
+//         products: [
+//             { title: "react.js", price: "99$" },
+//             { title: "node.js", price: "89$" },
+//             { title: "javascript", price: "79$" }
+//         ],
+//     };
+
+//     clickHandler =()=> {
+//         this.setState({
+//                 products:[
+//                     { title: "react.js", price: "79$" },
+//                     { title: "node.js", price: "69$" },
+//                     { title: "javascript", price: "59$" },
+//                 ],
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <div className="container" id="title">
+//                 <h1>shopping</h1>
+//                 {this.state.products.map((product) => {
+//                     return <Product name={product.title} price={product.price} />
+//                 })}
+//                 <button onClick={this.clickHandler}>change price</button>
+//             </div >
+//         );
+//     }
+// }
+// export default App
+
+
