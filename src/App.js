5595ad681af83c5652ba3b3ac20d25c77bb75aa9
behrooz <behrooz.zendedel1992@gmail.com>
2022-02-18 16:50:28 +0330

    show hookcounter to app

--- src/App.js
+++ src/App.js
@@ -1,35 +1,47 @@
 import React, { useState } from 'react';
 import Product from './components/Product/Product';
-
+import HookCounter from './components/HookCounter';
 
 
 const App = () => {
-  const [products,setProducts]=  useState([
-                    { title: "react.js", price: "99$" },
-                    { title: "node.js", price: "89$" },
-                    { title: "javascript", price: "79$" }
-                ]);
-
-                const clickHandler =()=>{
-                    setProducts([
-                        { title: "react.js", price: "79$" },
-                        { title: "node.js", price: "69$" },
-                        { title: "javascript", price: "59$" }
-                    ])
-                }
-
-    return (
-        <div className="container" id="title">
-            <h1>shopping</h1>
-            {products.map((product) => {
-                return <Product name={product.title} price={product.price} />
-            })}
-            <button onClick={clickHandler}>change price</button>
-        </div >
-    );
+    return(
+        <div>
+        <HookCounter />
+    </div>
+    )
+    
 };
 
 export default App;
+
+
+
+
+//   const [products,setProducts]=  useState([
+//                     { title: "react.js", price: "99$" },
+//                     { title: "node.js", price: "89$" },
+//                     { title: "javascript", price: "79$" }
+//                 ]);
+
+//                 const clickHandler =()=>{
+//                     setProducts([
+//                         { title: "react.js", price: "79$" },
+//                         { title: "node.js", price: "69$" },
+//                         { title: "javascript", price: "59$" }
+//                     ])
+//                 }
+
+//     return (
+//         <div className="container" id="title">
+//             <h1>shopping</h1>
+//             {products.map((product) => {
+//                 return <Product name={product.title} price={product.price} />
+//             })}
+//             <button onClick={clickHandler}>change price</button>
+//         </div > );
+      
+
+
 
 
 
