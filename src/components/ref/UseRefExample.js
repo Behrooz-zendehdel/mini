5a2fda8fce3e6e0703c60d09e329c9e8c8e9e5da
behrooz <behrooz.zendedel1992@gmail.com>
2022-02-22 21:28:03 +0330

    update ref

--- src/components/ref/UseRefExample.js
+++ src/components/ref/UseRefExample.js
@@ -1,18 +1,47 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const UseRefExample = () => {
     const [inputValue, setInputValue] = useState("");
+    const [count, setCount] = useState(0);
+    const inputRef = useRef();
+    const previousValue = useRef();
+    const previuosCount = useRef();
+
+
+    useEffect(() => {
+        previuosCount.current = count;
+    }, [count])
+
+    useEffect(() => {
+
+        previousValue.current = inputValue; // does not rerender component!!
+    }, [inputValue])
+
 
     const changeHandler = (e) => {
         setInputValue(e.target.value);
     }
-    const clickHandler = () => {
+
+
+
+    const resetHandler = () => {
         setInputValue("")
+        inputRef.current.focus();
     }
+
+
     return (
         <div>
-            <input type="text" onChange={changeHandler} />
-            <button onClick={clickHandler}>reset</button>
+            <input type="text" value={inputValue} onChange={changeHandler} />
+            <button onClick={resetHandler}>reset</button>
+            <p>my name is input value and it use to be {previousValue.current}</p>
+            <button onClick={() => setCount(Math.ceil(Math.random() * 10))}>
+                Generate random
+            </button>
+            <div>
+                count is :{count}
+                <div>previuoscount :{previuosCount.current}</div>
+            </div>
         </div>
     );
 }
